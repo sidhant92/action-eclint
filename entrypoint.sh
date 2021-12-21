d4bce2385c8874f440cda454546816f58391b4e6
Sidhant Aggarwal <10743214+sidhant92@users.noreply.github.com>
2021-12-21 18:26:15 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,5 +13,5 @@ JENKINS_URL=1 BUILD_ID=1 CI_REPORTS="$TARGET_FOLDER" \
   eclint check ${INPUT_ECLINT_FLAGS:-'.'} 2>/dev/null
 
 # Extract result from file or fallback if file does not exist
-(cat "$TARGET_FOLDER/*/checkstyle-result.xml" 2>/dev/null || echo '<checkstyle></checkstyle>') | \
+(cat $TARGET_FOLDER/*/checkstyle-result.xml 2>/dev/null || echo '<checkstyle></checkstyle>') | \
   reviewdog -f=checkstyle -name="eclint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
